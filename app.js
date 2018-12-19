1c29c2960ef73daba0b3649111d854c91c0ccd5b
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-12-19 00:42:12 +0000

    version16.0

--- app.js
+++ app.js
@@ -30,9 +30,34 @@ addForm.addEventListener("submit", function(e){
     bookName.classList.add("name");
     deleteBtn.classList.add("delete");
     
+    // hide books
+    const hideBox = document.querySelector("#hide");
+    hideBox.addEventListener("change", function(e){
+        if(hideBox.checked){
+            list.style.display = "none";
+        } else {
+            list.style.display = "block";
+        }
+    });
+    
+    // filter books
+    const searchBar = document.forms("search-books").querySelector("input");
+    searchBar.addEventListener("keyup", function(e){
+        const term = e.target.value.toLowerCase();
+        const books = list.getElementsByTagName("li");
+        Array.from(books).forEach(function(book){
+            const title = book.firstElementChild.textContent;
+            if(title.toLowerCase().indexOf(term) != -1) {
+                book.style.display = "block";
+            } else {
+                book.style.display = "none";
+            }
+        })
+    });
+    
     // append to document
-    li.appendChild(bookName);
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
+    // li.appendChild(bookName);
+    // li.appendChild(deleteBtn);
+    // list.appendChild(li);
 })
 
